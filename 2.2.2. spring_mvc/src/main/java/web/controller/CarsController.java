317341ad19fad6c07456ed89cdd5ed88d5b40a5e
vladeg1977 <73729840+vladeg1977@users.noreply.github.com>
2021-02-09 14:23:46 +0700

    service

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -1,14 +1,21 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.CarService;
+import web.service.CarService;
 
 @Controller
 public class CarsController {
-    CarService carService = new CarService();
+    private final CarService carService;
+
+    @Autowired
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(required = false) Integer count, Model model) {
         model.addAttribute("cars", carService.getCars(count));
